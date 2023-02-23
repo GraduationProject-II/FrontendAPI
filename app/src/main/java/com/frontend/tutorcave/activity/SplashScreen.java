b9f4b869487fefe1447d517b9b7b1e154366f635
root113 <vsametustun@gmail.com>
2023-02-23 16:50:35 +0300

    Splash screen implementation

---
+++ app/src/main/java/com/frontend/tutorcave/activity/SplashScreen.java
@@ -0,0 +1,90 @@
+package com.frontend.tutorcave.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.frontend.tutorcave.R;
+
+import java.util.Objects;
+
+//* Copyright (c) 2022, Samet Vural Üstün, All rights reserved.
+/**@author Samet Vural Üstün
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+    private LinearLayout layout;
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // assign components
+        CompModel components = findComponent(layout, appName);
+        layout = components.getLinearLayout();
+        appName = components.getTextView();
+
+        Animation splashAnim = AnimationUtils.loadAnimation(this, R.anim.transition);
+        Animation textAnim = AnimationUtils.loadAnimation(this, R.anim.to_left);
+        // TODO: load text anim first
+        appName.startAnimation(textAnim);
+        appName.startAnimation(splashAnim);
+        layout.startAnimation(splashAnim);
+
+        final Intent intent = new Intent(this, WelcomeActivity.class);
+
+        Thread timer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException exception) {
+                    Objects.requireNonNull(exception.getCause()).getMessage();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    // UTIL
+    private CompModel findComponent(LinearLayout layout, TextView textView) {
+        layout = (LinearLayout) findViewById(R.id.lytSplashBG);
+        textView = (TextView) findViewById(R.id.txtSplashMain);
+        CompModel components = new CompModel();
+        components.setLinearLayout(layout);
+        components.setTextView(textView);
+        return components;
+    }
+}
+
+class CompModel {
+    private LinearLayout linearLayout;
+    private TextView textView;
+
+    public LinearLayout getLinearLayout() {
+        return linearLayout;
+    }
+
+    public void setLinearLayout(LinearLayout linearLayout) {
+        this.linearLayout = linearLayout;
+    }
+
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public void setTextView(TextView textView) {
+        this.textView = textView;
+    }
+}
--- app/src/main/java/com/frontend/tutorcave/activity/WelcomeActivity.java
+++ app/src/main/java/com/frontend/tutorcave/activity/WelcomeActivity.java
@@ -1,14 +1,16 @@
-package com.frontend.tutorcave;
+package com.frontend.tutorcave.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.frontend.tutorcave.R;
+
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }

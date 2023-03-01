f382f792804d96a22c6128248a9b3c75003d2e72
root113 <vsametustun@gmail.com>
2023-03-01 19:03:29 +0300

    Search Tutor menu fragment design implementation

--- app/src/main/java/com/frontend/tutorcave/fragment/MenuSearchTutorFragment.java
+++ app/src/main/java/com/frontend/tutorcave/fragment/MenuSearchTutorFragment.java
@@ -9,13 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.frontend.tutorcave.R;
+import com.google.android.material.chip.Chip;
 
 //* Copyright (c) 2022, Samet Vural Üstün, All rights reserved.
 /** @author Samet Vural Üstün */
 
 public class MenuSearchTutorFragment extends Fragment {
+
+    // TODO: searchView.clearFocus(), onQueryTextListener()
+    private Chip chipTopRated, chipTrending, chipNewcomer;
 
     // TODO: set anim
 
@@ -46,7 +52,37 @@ public class MenuSearchTutorFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        // implementation
+
+        chipTopRated = (Chip) requireView().findViewById(R.id.frgSearchTutorChipTopRated);
+        chipTrending = (Chip) requireView().findViewById(R.id.frgSearchTutorChipTrending);
+        chipNewcomer = (Chip) requireView().findViewById(R.id.frgSearchTutorChipNewcomers);
+
+        // 1st Chip
+        chipTopRated.setOnCheckedChangeListener((cmpButtonView, isChecked) -> {
+            if (isChecked) {
+                // TODO: test purposes, delete later
+                Toast.makeText(MenuSearchTutorFragment.this.getContext(), R.string.test1, Toast.LENGTH_SHORT).show();
+            }
+            // TODO: add else statement
+        });
+
+        // 2nd Chip
+        chipTrending.setOnCheckedChangeListener((cmpButtonView, isChecked) -> {
+            if (isChecked) {
+                // TODO: test purposes, delete later
+                Toast.makeText(MenuSearchTutorFragment.this.getContext(), R.string.test2, Toast.LENGTH_SHORT).show();
+            }
+            // TODO: add else statement
+        });
+
+        //3rd Chip
+        chipNewcomer.setOnCheckedChangeListener((cmpButtonView, isChecked) -> {
+            if (isChecked) {
+                // TODO: test purposes, delete later
+                Toast.makeText(MenuSearchTutorFragment.this.getContext(), R.string.test3, Toast.LENGTH_SHORT).show();
+            }
+            // TODO: add else statement
+        });
     }
 
     @Override
